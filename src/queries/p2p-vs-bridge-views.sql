071297dbc2415ba7da5bac4c39267d63e62c30f5
George Politis <georgios.politis@8x8.com>
2022-02-11 13:16:12 +0000

    Update p2p-vs-bridge-views.sql
    Renames the result fields

--- src/queries/p2p-vs-bridge-views.sql
+++ src/queries/p2p-vs-bridge-views.sql
@@ -1,12 +1,12 @@
 SELECT appenv, isp2p,
-    COUNT(sentpacketslostpct) AS count_samples,
-    AVG(sentpacketslostpct) AS avg_sentpacketslostpct,
-    AVG(receivedpacketslostpct) AS avg_receivedpacketslostpct,
-    AVG(meanrtt) AS avg_meanrtt
+	COUNT(sentpacketslostpct) AS samples_count,
+    AVG(sentpacketslostpct) AS sentpacketslostpct_avg,
+    AVG(receivedpacketslostpct) AS receivedpacketslostpct_avg,
+    AVG(meanrtt) AS meanrtt_avg
 FROM rtcstats_pc_metrics
 INNER JOIN rtcstats ON rtcstats.statssessionid = rtcstats_pc_metrics.statssessionid
 WHERE sessionduration >= 10000
-    AND sentpacketslostpct > 0
+	AND sentpacketslostpct > 0
     AND sentpacketslostpct < 100
     AND receivedpacketslostpct > 0
     AND receivedpacketslostpct < 100
