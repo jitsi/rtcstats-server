e93289959e5dea07962a0881ff9f43712b0bd354
George Politis <georgios.politis@8x8.com>
2022-02-11 13:12:50 +0000

    Update p2p-vs-bridge-views.sql
    Ignore non-sensical percentages from the quality comparison query.

--- src/queries/p2p-vs-bridge-views.sql
+++ src/queries/p2p-vs-bridge-views.sql
@@ -1,9 +1,13 @@
 SELECT appenv, isp2p,
-	COUNT(sentpacketslostpct) AS samples,
-    AVG(sentpacketslostpct) AS sentpacketslostpct,
-    AVG(receivedpacketslostpct) AS receivedpacketslostpct,
-    AVG(meanrtt) AS meanrtt
-FROM "rtcstats"."public"."rtcstats_pc_metrics"
+    COUNT(sentpacketslostpct) AS count_samples,
+    AVG(sentpacketslostpct) AS avg_sentpacketslostpct,
+    AVG(receivedpacketslostpct) AS avg_receivedpacketslostpct,
+    AVG(meanrtt) AS avg_meanrtt
+FROM rtcstats_pc_metrics
 INNER JOIN rtcstats ON rtcstats.statssessionid = rtcstats_pc_metrics.statssessionid
 WHERE sessionduration >= 10000
+    AND sentpacketslostpct > 0
+    AND sentpacketslostpct < 100
+    AND receivedpacketslostpct > 0
+    AND receivedpacketslostpct < 100
 GROUP BY isp2p, appenv
