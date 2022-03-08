f1a775ff838bc83778f44fe899bf20decc13a4b9
George Politis <georgios.politis@8x8.com>
2022-03-08 13:56:22 +0000

    Update p2p-vs-bridge-views.sql

--- src/queries/p2p-vs-bridge-views.sql
+++ src/queries/p2p-vs-bridge-views.sql
@@ -2,7 +2,9 @@ SELECT appenv, isp2p, usesrelay,
 	COUNT(sentpacketslostpct) AS samples_count,
     AVG(sentpacketslostpct) AS sentpacketslostpct_avg,
     AVG(receivedpacketslostpct) AS receivedpacketslostpct_avg,
-    AVG(meanrtt) AS meanrtt_avg
+    AVG(meanrtt) AS meanrtt_avg,
+    AVG(meanupperboundframeheight) AS meanupperboundframeheight_avg,
+    AVG(meanupperboundframespersecond) AS meanupperboundframespersecond_avg
 FROM rtcstats_pc_metrics
 INNER JOIN rtcstats ON rtcstats.statssessionid = rtcstats_pc_metrics.statssessionid
 WHERE sessionduration >= 10000
@@ -10,4 +12,6 @@ WHERE sessionduration >= 10000
     AND sentpacketslostpct < 100
     AND receivedpacketslostpct > 0
     AND receivedpacketslostpct < 100
+    AND isp2p IS NOT NULL -- filters out callstats peer connections
+    AND rtcstats_pc_metrics.createdate BETWEEN DATEADD(DAY,-3,GETDATE()) AND GETDATE() -- change this to something sensible or remove
 GROUP BY isp2p, appenv, usesrelay
